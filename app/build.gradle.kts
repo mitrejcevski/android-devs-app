829071b2646c1362810be82e0cff7bcccbaf448a
Jov Mit <mitrejcevski@gmail.com>
2023-10-05 17:57:09 +0200

    Target Andorid SDK 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "nl.jovmit.androiddevs"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "nl.jovmit.androiddevs"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
