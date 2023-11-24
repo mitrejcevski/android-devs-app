be80a031b793418cb87b6a87974d9ec4bf3084b0
Jov Mit <mitrejcevski@gmail.com>
2023-11-24 19:39:31 +0100

    Remove obsolete code

--- feature/login/src/main/java/nl/jovmit/androiddevs/feature/login/LoginViewModel.kt
+++ feature/login/src/main/java/nl/jovmit/androiddevs/feature/login/LoginViewModel.kt
@@ -11,18 +11,15 @@ class LoginViewModel @Inject constructor(
     private val savedStateHandle: SavedStateHandle
 ) : ViewModel() {
 
-    private val _screenState = MutableStateFlow(LoginScreenState())
     val screenState: StateFlow<LoginScreenState> =
         savedStateHandle.getStateFlow(LOGIN_SCREEN_STATE, LoginScreenState())
 
     fun updateEmail(newValue: String) {
-        _screenState.update { it.copy(email = newValue) }
         val value = savedStateHandle.get<LoginScreenState>(LOGIN_SCREEN_STATE)
         savedStateHandle[LOGIN_SCREEN_STATE] = value?.copy(email = newValue)
     }
 
     fun updatePassword(newValue: String) {
-        _screenState.update { it.copy(password = newValue) }
         val value = savedStateHandle.get<LoginScreenState>(LOGIN_SCREEN_STATE)
         savedStateHandle[LOGIN_SCREEN_STATE] = value?.copy(password = newValue)
     }
