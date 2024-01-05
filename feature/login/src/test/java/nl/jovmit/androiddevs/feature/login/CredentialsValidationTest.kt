9dea5b811818d7f93e3f3b4f00f18a365b2fdc11
Jov Mit <mitrejcevski@gmail.com>
2024-01-05 17:14:58 +0100

    Introduced proper email validation

--- feature/login/src/test/java/nl/jovmit/androiddevs/feature/login/CredentialsValidationTest.kt
+++ feature/login/src/test/java/nl/jovmit/androiddevs/feature/login/CredentialsValidationTest.kt
@@ -3,7 +3,6 @@ package nl.jovmit.androiddevs.feature.login
 import com.google.common.truth.Truth.assertThat
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
-import java.util.regex.Pattern
 
 class CredentialsValidationTest {
 
@@ -17,7 +16,7 @@ class CredentialsValidationTest {
         "'abc@de.f', false",
         "'abc@de.fe', true",
     )
-    fun incorrectEmail(email: String, expected: Boolean) {
+    fun emailValidation(email: String, expected: Boolean) {
         val emailValidator = EmailValidator()
 
         val result = emailValidator.validateEmail(email)
@@ -25,12 +24,4 @@ class CredentialsValidationTest {
         assertThat(result).isEqualTo(expected)
     }
 
-    class EmailValidator {
-
-        fun validateEmail(email: String): Boolean {
-            val regex = """[a-zA-Z0-9+._%\-]{1,64}@[a-zA-Z0-9][a-zA-Z0-9\-]{1,64}(\.[a-zA-Z0-9][a-zA-Z0-9\-]{1,25})"""
-            val emailPattern = Pattern.compile(regex)
-            return emailPattern.matcher(email).matches()
-        }
-    }
 }
