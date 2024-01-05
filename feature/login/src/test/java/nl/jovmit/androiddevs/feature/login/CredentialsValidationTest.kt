6a0ab5adf417df64ca08f29041c820713f931a8e
Jov Mit <mitrejcevski@gmail.com>
2024-01-05 17:05:46 +0100

    We can validate emails by using regex

---
+++ feature/login/src/test/java/nl/jovmit/androiddevs/feature/login/CredentialsValidationTest.kt
@@ -0,0 +1,30 @@
+package nl.jovmit.androiddevs.feature.login
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import java.util.regex.Pattern
+
+class CredentialsValidationTest {
+
+    @ParameterizedTest
+    @CsvSource(
+        "'   ', false",
+        "' a  ', false",
+        "' ab  ', false",
+        "'ab@c', false",
+        "'abc@d', false",
+        "'abc@de.f', false",
+        "'abc@de.fe', true",
+    )
+    fun incorrectEmail(email: String, expected: Boolean) {
+        val result = validateEmail(email)
+        assertThat(result).isEqualTo(expected)
+    }
+    
+    private fun validateEmail(email: String): Boolean {
+        val regex = """[a-zA-Z0-9+._%\-]{1,64}@[a-zA-Z0-9][a-zA-Z0-9\-]{1,64}(\.[a-zA-Z0-9][a-zA-Z0-9\-]{1,25})"""
+        val emailPattern = Pattern.compile(regex)
+        return emailPattern.matcher(email).matches()
+    }
+}
