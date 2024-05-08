bfda3944b0c63e2846f6e85798bedb4a90a8cf14
Jovche Mitrejchevski <jovmit@Jovches-MacBook-Pro-2.local>
2024-05-08 13:47:04 +0200

    Provide coroutine dispatcher dependency

--- feature/login/src/main/java/nl/jovmit/androiddevs/feature/login/LoginModule.kt
+++ feature/login/src/main/java/nl/jovmit/androiddevs/feature/login/LoginModule.kt
@@ -4,6 +4,8 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import javax.inject.Singleton
 
 @Module
@@ -14,5 +16,11 @@ object LoginModule {
     @Singleton
     fun provideUserCatalog(): UsersCatalog {
         return InMemoryUsersCatalog(emptyMap())
+    }
+
+    @Provides
+    @Singleton
+    fun provideBackgroundDispatcher(): CoroutineDispatcher {
+        return Dispatchers.IO
     }
 }
