98cbc62714dc0cf8873624ce4ff0c29ce2e6d212
Jovche Mitrejchevski <mitrejcevski@gmail.com>
2024-07-05 18:31:58 +0200

    Additional setup to force logout

--- feature/timeline/build.gradle.kts
+++ feature/timeline/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose.compiler)
+    alias(libs.plugins.hilt.android)
+    alias(libs.plugins.kapt)
 }
 
 android {
@@ -52,6 +54,10 @@ android {
 
 dependencies {
     implementation(projects.core.view)
+    implementation(projects.core.network)
+    implementation(libs.bundles.hilt)
+
+    kapt(libs.hilt.compiler)
 
     testImplementation(libs.bundles.unit.testing)
 
