7d66ade310c76a5f6733aa7925ae9043f65dda37
Jovche Mitrejchevski <mitrejcevski@gmail.com>
2024-09-04 18:31:47 +0200

    Introduce the basic sign up form

--- feature/signup/src/main/java/nl/jovmit/androiddevs/feature/signup/SignUpScreen.kt
+++ feature/signup/src/main/java/nl/jovmit/androiddevs/feature/signup/SignUpScreen.kt
@@ -1,8 +1,16 @@
 package nl.jovmit.androiddevs.feature.signup
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -15,7 +23,18 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.PreviewLightDark
+import nl.jovmit.androiddevs.core.view.R
+import nl.jovmit.androiddevs.core.view.composables.EmailInput
+import nl.jovmit.androiddevs.core.view.composables.PasswordInput
+import nl.jovmit.androiddevs.core.view.composables.PrimaryButton
+import nl.jovmit.androiddevs.core.view.composables.TextInput
 import nl.jovmit.androiddevs.core.view.theme.AppTheme
 
 @Composable
@@ -23,6 +42,9 @@ internal fun SignUpScreen(
     onNavigateUp: () -> Unit
 ) {
     SignUpScreenContent(
+        onSignUp = {
+            //TODO call view model
+        },
         onNavigateUp = onNavigateUp
     )
 }
@@ -30,6 +52,7 @@ internal fun SignUpScreen(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun SignUpScreenContent(
+    onSignUp: () -> Unit,
     onNavigateUp: () -> Unit,
 ) {
     Scaffold(
@@ -59,16 +82,77 @@ private fun SignUpScreenContent(
             )
         }
     ) { paddingValues ->
-        Box(
+        val verticalScroll = rememberScrollState()
+        Column(
             modifier = Modifier
                 .fillMaxSize()
+                .verticalScroll(verticalScroll)
                 .padding(paddingValues),
-            contentAlignment = Alignment.Center
+            verticalArrangement = Arrangement.spacedBy(AppTheme.size.large)
         ) {
-            Text(
-                text = "Hi from Sign Up!",
-                color = AppTheme.colorScheme.onBackground
+            Image(
+                modifier = Modifier
+                    .padding(AppTheme.size.large)
+                    .align(Alignment.CenterHorizontally),
+                painter = painterResource(id = R.drawable.logo_android_devs),
+                contentDescription = stringResource(id = R.string.cd_logo)
             )
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .imePadding()
+                    .padding(horizontal = AppTheme.size.medium),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(AppTheme.size.normal)
+            ) {
+                val passwordFocus = FocusRequester()
+                val aboutFocus = FocusRequester()
+                EmailInput(
+                    modifier = Modifier.fillMaxWidth(),
+                    email = "",
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Email,
+                        imeAction = ImeAction.Next
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onNext = { passwordFocus.requestFocus() }
+                    ),
+                    onEmailChanged = {}
+                )
+                PasswordInput(
+                    modifier = Modifier
+                        .focusRequester(passwordFocus)
+                        .fillMaxWidth(),
+                    keyboardOptions = KeyboardOptions(
+                        imeAction = ImeAction.Next
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onNext = { aboutFocus.requestFocus() }
+                    ),
+                    password = "",
+                    onPasswordChanged = {}
+                )
+                TextInput(
+                    modifier = Modifier
+                        .focusRequester(aboutFocus)
+                        .fillMaxWidth(),
+                    text = "",
+                    onTextChanged = {},
+                    keyboardOptions = KeyboardOptions(
+                        imeAction = ImeAction.Done
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onDone = { onSignUp() }
+                    ),
+                    label = "About",
+                    hint = "Bio"
+                )
+                PrimaryButton(
+                    modifier = Modifier.fillMaxWidth(),
+                    label = stringResource(R.string.sign_up_title),
+                    onClick = onSignUp
+                )
+            }
         }
     }
 }
@@ -78,6 +162,7 @@ private fun SignUpScreenContent(
 private fun PreviewLoginScreen() {
     AppTheme {
         SignUpScreenContent(
+            onSignUp = {},
             onNavigateUp = {}
         )
     }
