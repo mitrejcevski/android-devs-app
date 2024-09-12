32e7c76a42ef831d3eb5a931e338ffa5f152001e
Jovche Mitrejchevski <mitrejcevski@gmail.com>
2024-09-12 09:02:55 +0200

    Rename auto-generated test

--- feature/signup/src/test/java/nl/jovmit/androiddevs/feature/signup/SignUpScreenStateTest.kt
+++ feature/signup/src/test/java/nl/jovmit/androiddevs/feature/signup/SignUpScreenStateTest.kt
@@ -3,12 +3,7 @@ package nl.jovmit.androiddevs.feature.signup
 import com.google.common.truth.Truth.assertThat
 import org.junit.jupiter.api.Test
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-class ExampleUnitTest {
+class SignUpScreenStateTest {
 
     @Test
     fun addition_isCorrect() {
