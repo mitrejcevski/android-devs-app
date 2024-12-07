3662fd227f6666630abe3414b7ca6c9842455e70
Jovche Mitrejchevski <mitrejcevski@gmail.com>
2024-12-07 08:24:30 +0100

    Remove unused imports

--- app/src/main/java/nl/jovmit/androiddevs/MainActivity.kt
+++ app/src/main/java/nl/jovmit/androiddevs/MainActivity.kt
@@ -4,12 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import dagger.hilt.android.AndroidEntryPoint
 import nl.jovmit.androiddevs.core.view.theme.AppTheme
 
