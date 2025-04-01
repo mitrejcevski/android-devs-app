c1bde2c67e7f3a0f773b149c9208fb136c019e4b
Jovche Mitrejchevski <mitrejcevski@gmail.com>
2025-04-01 17:37:19 +0200

    Uncomment screenshot tests

--- feature/login/src/test/java/nl/jovmit/androiddevs/feature/login/LoginScreenshotTest.kt
+++ feature/login/src/test/java/nl/jovmit/androiddevs/feature/login/LoginScreenshotTest.kt
@@ -1,42 +1,42 @@
-//package nl.jovmit.androiddevs.feature.login
-//
-//import app.cash.paparazzi.DeviceConfig
-//import app.cash.paparazzi.Paparazzi
-//import nl.jovmit.androiddevs.core.view.theme.AppTheme
-//import org.junit.Rule
-//import org.junit.Test
-//
-//class LoginScreenshotTest {
-//
-//    @get:Rule
-//    val paparazzi = Paparazzi(
-//        deviceConfig = DeviceConfig.PIXEL_5
-//    )
-//
-//    @Test
-//    fun defaultLoginScreen() {
-//        paparazzi.snapshot {
-//            AppTheme {
-//                LoginScreenContent(
-//                    screenState = LoginScreenState(),
-//                    loginActions = EmptyLoginActions(),
-//                    onNavigateUp = {}
-//                )
-//            }
-//        }
-//    }
-//
-//    private class EmptyLoginActions: LoginActions {
-//        override fun updateEmail(newValue: String) {
-//            TODO("Not yet implemented")
-//        }
-//
-//        override fun updatePassword(newValue: String) {
-//            TODO("Not yet implemented")
-//        }
-//
-//        override fun login() {
-//            TODO("Not yet implemented")
-//        }
-//    }
-//}
+package nl.jovmit.androiddevs.feature.login
+
+import app.cash.paparazzi.DeviceConfig
+import app.cash.paparazzi.Paparazzi
+import nl.jovmit.androiddevs.core.view.theme.AppTheme
+import org.junit.Rule
+import org.junit.Test
+
+class LoginScreenshotTest {
+
+    @get:Rule
+    val paparazzi = Paparazzi(
+        deviceConfig = DeviceConfig.PIXEL_5
+    )
+
+    @Test
+    fun defaultLoginScreen() {
+        paparazzi.snapshot {
+            AppTheme {
+                LoginScreenContent(
+                    screenState = LoginScreenState(),
+                    loginActions = EmptyLoginActions(),
+                    onNavigateUp = {}
+                )
+            }
+        }
+    }
+
+    private class EmptyLoginActions: LoginActions {
+        override fun updateEmail(newValue: String) {
+            TODO("Not yet implemented")
+        }
+
+        override fun updatePassword(newValue: String) {
+            TODO("Not yet implemented")
+        }
+
+        override fun login() {
+            TODO("Not yet implemented")
+        }
+    }
+}
