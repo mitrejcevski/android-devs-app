21a7887f1488b41a260192fb3e2d1bf3562d5a39
Jov Mit <mitrejcevski@gmail.com>
2026-05-18 21:22:22 +0200

    Allow formatting dates from long

--- shared/ui/src/main/java/nl/jovmit/androiddevs/shared/ui/datetime/AppDateTimeFormat.kt
+++ shared/ui/src/main/java/nl/jovmit/androiddevs/shared/ui/datetime/AppDateTimeFormat.kt
@@ -3,4 +3,6 @@ package nl.jovmit.androiddevs.shared.ui.datetime
 interface AppDateTimeFormat {
 
     fun toDateTime(zonedDateTime: String): String
+
+    fun toDateTime(dateTimeMillis: Long): String
 }
--- shared/ui/src/main/java/nl/jovmit/androiddevs/shared/ui/datetime/ZonedAppDateTimeFormat.kt
+++ shared/ui/src/main/java/nl/jovmit/androiddevs/shared/ui/datetime/ZonedAppDateTimeFormat.kt
@@ -2,17 +2,22 @@ package nl.jovmit.androiddevs.shared.ui.datetime
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.staticCompositionLocalOf
+import java.time.Instant
 import java.time.ZoneId
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 
-internal val LocalDateTimeFormat = staticCompositionLocalOf<AppDateTimeFormat> {
+internal val LocalDateTimeFormat = staticCompositionLocalOf {
     passByFormat
 }
 
 internal val passByFormat = object : AppDateTimeFormat {
     override fun toDateTime(zonedDateTime: String): String {
         return zonedDateTime
+    }
+
+    override fun toDateTime(dateTimeMillis: Long): String {
+        return "$dateTimeMillis"
     }
 }
 
@@ -23,6 +28,13 @@ internal val appZonedDateTimeFormat = object : AppDateTimeFormat {
             .withZoneSameInstant(ZoneId.systemDefault())
             .toLocalDateTime()
         return DateTimeFormatter.ofPattern("DD MM yyyy").format(date)
+    }
+
+    override fun toDateTime(dateTimeMillis: Long): String {
+        val dateTime = Instant.ofEpochMilli(dateTimeMillis)
+            .atZone(ZoneId.systemDefault())
+            .toLocalDateTime()
+        return DateTimeFormatter.ofPattern("DD MM yyyy").format(dateTime)
     }
 }
 
